0c725023d0032d260872b4a4d3ccf21bcef3b1b1
Marina <marina@Marinas-MacBook-Pro.local>
2022-08-08 15:46:47 +0300

    ...

---
+++ app/src/main/java/com/disney/emojimatch_go/white/Game.kt
@@ -0,0 +1,12 @@
+package com.disney.emojimatch_go.white
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.disney.emojimatch_go.R
+
+class Game : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+}
